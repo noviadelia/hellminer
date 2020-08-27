04038c9430dd1f50ec5ff798eff7d5eb3f7fe5a2
noviadelia <70348306+noviadelia@users.noreply.github.com>
2020-08-28 04:25:25 +0700

    Add files via upload

---
+++ hellminer/mine-hybrid-staticdiff.sh
@@ -0,0 +1,1 @@
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RQHcgTvC41tAvUt38YxivSvVgdLS8NFFAf.hellminer -p d=16384s,hybrid --cpu 3
---
+++ hellminer/mine.sh
@@ -0,0 +1,1 @@
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RQHcgTvC41tAvUt38YxivSvVgdLS8NFFAf.hellminer -p x --cpu 3
